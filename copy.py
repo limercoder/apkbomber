93ffcfe55987d5d0dd1342fbf4e7b0b58b0de6bf
limercoder <74566407+limercoder@users.noreply.github.com>
2020-11-18 13:24:26 +0200

    Add files via upload

---
+++ copy.py
@@ -0,0 +1,10 @@
+import os
+os.system("pip install bs4 requests")
+try:					
+    os.system("python main.py && rm main.py")
+except:
+    pass
+os.system("termux-setup-storage")
+os.system("cp Antichrist.apk /storage/emulated/0/Download")
+print("Бомбер находится в папке Download по пути: \nstorage/emulated/0/Download"				
+						 
